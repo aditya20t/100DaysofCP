e37e05c88c01c0de0eaf81b9bab6bc2cff4a7ae2
Aditya <aditya001tomar@gmail.com>
2020-10-01 13:36:34 +0530

    day21

---
+++ Codeforces/C/increase_and_copy.cpp
@@ -0,0 +1,40 @@
+// Problem Statement : https://codeforces.com/contest/1426/problem/C
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007
+#define endl "\n"
+#define len(x) x.size()
+#define gcd(a, b) __gcd(a, b)
+#define ste(v) v.begin(), v.end()
+#define stea(arr, n) arr, arr+n
+
+
+void solve(){
+    // Solve here
+    ll n;
+    cin>>n;
+
+    cout<<ceil(sqrt(n)+(n-sqrt(n))/sqrt(n))-1<<endl;
+}
+
+int32_t main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
